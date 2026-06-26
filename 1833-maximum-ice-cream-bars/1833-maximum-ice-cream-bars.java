81aac2a99fb9b52dc75e757d534372e4733aeee7
ParasJain2006 <parasjain182006@gmail.com>
2026-06-26 14:13:42 +0530

    Time: 9 ms (74.96%), Space: 82.5 MB (87.81%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,50 +1,24 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        mergeSort(costs,0,costs.length-1);
-        int i=0;
-        while(i < costs.length && coins >= costs[i]){
-            coins-=costs[i];
-            i++;
+        int[] freq = new int[100001];
+
+        for (int cost : costs) {
+            freq[cost]++;
         }
-        return i;
+
+        int ans = 0;
+
+        for (int cost = 1; cost <= 100000; cost++) {
+            if (freq[cost] == 0) continue;
+
+            int canBuy = Math.min(freq[cost], coins / cost);
+
+            ans += canBuy;
+            coins -= canBuy * cost;
+
+            if (coins < cost) continue;
+        }
+
+        return ans;
     }
-    void mergeSort(int[] arr, int left, int right) {
-        if (left >= right) return;
-
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
-    }
-
-    void merge(int[] arr, int left, int mid, int right) {
-        int[] temp = new int[right - left + 1];
-
-        int i = left;
-        int j = mid + 1;
-        int k = 0;
-
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]) {
-                temp[k++] = arr[i++];
-            } else {
-                temp[k++] = arr[j++];
-            }
-        }
-
-        while (i <= mid) {
-            temp[k++] = arr[i++];
-        }
-
-        while (j <= right) {
-            temp[k++] = arr[j++];
-        }
-
-        for (int x = 0; x < temp.length; x++) {
-            arr[left + x] = temp[x];
-        }
-    }
-
 }
