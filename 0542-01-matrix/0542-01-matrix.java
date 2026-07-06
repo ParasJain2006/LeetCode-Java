a8be965463de75e41f8d69ff73df9cbc19f11416
ParasJain2006 <parasjain182006@gmail.com>
2026-07-07 05:23:42 +0530

    Time: 16 ms (27.43%), Space: 49.8 MB (11%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,35 +1,33 @@
-import java.util.*;
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int rows = mat.length, cols = mat[0].length;
-        int[][] ans = new int[rows][cols];
-        boolean[][] visited = new boolean[rows][cols];
-        Queue<int[]> q = new LinkedList<>();
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (mat[i][j] == 0) {
-                    q.offer(new int[] {i, j});
-                    visited[i][j] = true;
+        int r=mat.length,c=mat[0].length;
+        boolean[][] visited= new boolean[r][c];
+        int[][] ans= new int[r][c];
+        Queue<int[]> q= new LinkedList<>();
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(mat[i][j]==0) {
+                    q.add(new int[]{i,j,0});
+                    visited[i][j]=true;
                 }
             }
         }
-        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-        int dist = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int s = 0; s < size; s++) {
-                int[] cell = q.poll();
-                int r = cell[0], c = cell[1];
-                ans[r][c] = dist;
-                for (int[] d : dirs) {
-                    int nr = r + d[0], nc = c + d[1];
-                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && !visited[nr][nc]) {
-                        visited[nr][nc] = true;
-                        q.offer(new int[] {nr, nc});
-                    }
+        int dirs[][]={{-1,0},{0,-1},{1,0},{0,1}};
+
+        while(!q.isEmpty()){
+            int[] temp=q.poll();
+            int i=temp[0];
+            int j=temp[1];
+            int k=temp[2];
+            ans[i][j]=k;
+            for(int[] x:dirs){
+                int ni=i+x[0];
+                int nj=j+x[1];
+                if(ni>=0 && nj>=0 && ni<r && nj<c && !visited[ni][nj]){
+                        q.add(new int[]{ni,nj,k+1});
+                        visited[ni][nj]=true;
                 }
             }
-            dist++;
         }
         return ans;
     }
