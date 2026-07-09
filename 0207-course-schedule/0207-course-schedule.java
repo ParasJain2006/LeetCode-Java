ebd0d55d4430daf61bfbbbc423f55dc9c81d64f6
ParasJain2006 <parasjain182006@gmail.com>
2026-07-09 15:52:56 +0530

    Time: 6 ms (72.23%), Space: 46.8 MB (88.37%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,42 +1,25 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
+        Queue<Integer> q= new LinkedList<>();
         List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
-            adj.add(new ArrayList<>());
-        }
-
-        int[] indegree = new int[numCourses];
-
-        // Build graph and calculate indegree
-        for (int[] edge : prerequisites) {
+        int[] in= new int[numCourses];
+        for(int i=0;i<numCourses;i++) adj.add(new ArrayList<>());
+        for(int[] edge: prerequisites){
             adj.get(edge[1]).add(edge[0]);
-            indegree[edge[0]]++;
+            in[edge[0]]++;
         }
-
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.offer(i);
+        for(int i=0;i<in.length;i++){
+            if(in[i]==0) q.add(i);
+        }
+        int check=0;
+        while(!q.isEmpty()){
+            int x=q.poll();
+            check++;
+            for(int i:adj.get(x)){
+                in[i]--;
+                if(in[i]==0) q.add(i);
             }
         }
-
-        int count = 0;
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            count++;
-
-            for (int next : adj.get(node)) {
-                indegree[next]--;
-
-                if (indegree[next] == 0) {
-                    q.offer(next);
-                }
-            }
-        }
-
-        return count == numCourses;
+        return check==numCourses;
     }
 }
